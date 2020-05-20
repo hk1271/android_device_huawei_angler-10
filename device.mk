a6e2ae96996572cf8ad47a6c663ae264a7c68cc5
hk1271 <62790149+hk1271@users.noreply.github.com>
2020-05-20 17:05:22 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -396,7 +396,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Write Manufacturer & Model information in created media files.
 # IMPORTANT: ONLY SET THIS PROPERTY TO TRUE FOR PUBLIC DEVICES
-ifneq ($(filter xtended_angler% angler%, $(TARGET_PRODUCT)),)
+ifneq ($(filter aosp_angler% angler%, $(TARGET_PRODUCT)),)
 PRODUCT_PROPERTY_OVERRIDES += \
     media.recorder.show_manufacturer_and_model=true
 else
@@ -545,7 +545,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Modem debugger
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-ifeq (,$(filter xtended_angler, $(TARGET_PRODUCT)))
+ifeq (,$(filter aosp_angler, $(TARGET_PRODUCT)))
 PRODUCT_PACKAGES += \
     NexusLogger
 endif # aosp_angler
