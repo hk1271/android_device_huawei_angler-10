e3df6673e8f58175890e9c7b523a3956a0bc49f7
hk1271 <62790149+hk1271@users.noreply.github.com>
2020-05-20 17:01:21 +0530

    du-ify

--- du.mk
+++ du.mk
@@ -37,5 +37,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:8.1.0/OPM6.171019.030.H1/4903069:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user OPM6.171019.030.H1 4903069 release-keys"
+    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM3.171019.014 4503998 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/angler/angler:8.1.0/OPM3.171019.014/4503998:user/release-keys
+
+BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM3.171019.014/4503998:user/release-keys
